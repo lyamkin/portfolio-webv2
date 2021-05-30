b1e686d5d238ab0576da78f2833089ee871b4f5d
Andrey Lyamkin <andrew.lyamkin@gmail.com>
2021-05-29 21:08:30 -0400

    Initial setup

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+session_start();
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+if (strpos($_SERVER['HTTP_HOST'], "alyamkin.ca") !== false) {
+    // hosting on ipd24.ca
+    DB::$dbName = 'cp5003_portfolio';
+    DB::$user = 'cp5003_portfolio';
+    DB::$password = 'Zsu-nB.r8xD2oXbf';
+} else { // local computer
+    DB::$dbName = 'portfolio';
+    DB::$user = 'portfolio';
+    DB::$password = 'Zsu-nB.r8xD2oXbf';
+    DB::$port = 3333;
+}
+
+    // DB::$dbName = 'portfolio';
+    // DB::$user = 'portfolio';
+    // DB::$password = 'Zsu-nB.r8xD2oXbf';
+    // DB::$port = 3333;
+
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+    'displayErrorDetails' => true
+]];
+$app = new \Slim\App($config);
+
+// Fetch DI Container
+$container = $app->getContainer();
+$container['upload_directory'] = __DIR__ . '/uploads';
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig(dirname(__FILE__) . '/templates', [
+        'cache' => dirname(__FILE__) . '/tmplcache',
+        'debug' => true, // This line should enable debug mode
+    ]);
+    //
+    $view->getEnvironment()->addGlobal('test1','VALUE');
+    // Instantiate and add Slim specific extension
+    $router = $c->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+    return $view;
+};
+
+// for index.html.twig:
+$app->get('/', function (Request $request, Response $response, $args) {
+    $projects = DB::query("SELECT * FROM projects ORDER BY date DESC");
+    return $this->view->render($response, 'index.html.twig', ['projects' => $projects]);
+});
+// load details ajax
+$app->get('/ajaxgetdetails/{id:[0-9]+}', function (Request $request, Response $response, $args) {
+    $details = DB::query("SELECT * FROM details WHERE projectId=%s", $args['id']);
+    return $this->view->render($response, 'project-details.html.twig', ['details' => $details]);
+});
+// load tools ajax
+$app->get('/ajaxgettools/{id:[0-9]+}', function (Request $request, Response $response, $args) {
+    $tools = DB::query("SELECT * FROM tools WHERE projectId=%s", $args['id']);
+    return $this->view->render($response, 'project-tools.html.twig', ['tools' => $tools]);
+});
+
+    
+$app->run();
+
+
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$("document").ready(function () {
+  // ajax request to load details for each project
+  // on button click
+  // $(".read-more-details").on("click", function () {
+  //   let projectId = $(this).data("detailsId");
+  //   $(`#detail-list-project-${projectId}`).load("/ajaxgetdetails/" + projectId);
+  // });
+  // on page load
+  $(".project-details").each(function (index, element) {
+    let projectId = $(this).data("detailListId");
+    $(this).load("/ajaxgetdetails/" + projectId);
+  });
+  // ajax request to load tools for each project
+  $(".project-tools").each(function (index, element) {
+    let projectId = $(this).data("toolsId");
+    $(this).load("/ajaxgettools/" + projectId);
+  });
+});
